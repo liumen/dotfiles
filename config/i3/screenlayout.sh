25a7b5a2c6df4a492c63662240e52375155cee4e
WU,Qihang <wu.qihang@hotmail.com>
2021-06-10 10:18:56 -0400

    massive update

--- config/i3/screenlayout.sh
+++ config/i3/screenlayout.sh
@@ -8,7 +8,7 @@
   # xrandr --output DVI-D-0 --primary --mode 2560x1600 --pos 0x0 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 2560x0 --rotate normal --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off
 # fi
 
-if [ $(hostname) == geodynamiker ] 
+if [ $(hostname) == geodynamiker ] || [ $(hostname) == qihangdesktop ]
 then
-  xrandr --output VGA-0 --mode 1920x1080 --pos 2560x0 --rotate left --output DVI-D-0 --primary --mode 2560x1600 --pos 0x0 --rotate normal --output HDMI-0 --off
+  xrandr --output HDMI-0 --mode 1920x1080 --pos 2560x0 --rotate normal --output DVI-D-0 --primary --mode 2560x1600 --pos 0x0 --rotate normal --output HDMI-0 --off
 fi
--- config/i3/wallpaper.sh
+++ config/i3/wallpaper.sh
@@ -6,7 +6,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # change wallpaper periodically
 while true 
 do
-  feh --randomize --bg-fill ~/.wallpaper/active/*
+  feh --randomize --bg-fill ~/.wallpaper/active/* --bg-fill ~/.wallpaper/active/*
   sleep 30m
 done
 
